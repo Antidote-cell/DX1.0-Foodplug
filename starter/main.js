7328c0eb4d4f52283bf19df8520a91128d815d6c
Antidote-cell <arotoluj@gmail.com>
2022-10-13 20:41:38 +0800

    idk

---
+++ starter/main.js
@@ -0,0 +1,17 @@
+
+
+// ES5 method
+
+// var writeup= "Antiidote exchange is active for all Crypto assests and Giftcards"
+
+// console.log (document)
+
+var hamburgerdiv = document.querySelector(".hamburger")
+
+console.log(hamburgerdiv)
+
+
+function dosomething() {
+    hamburgerdiv.classList.toggle("showburger")
+}
+
