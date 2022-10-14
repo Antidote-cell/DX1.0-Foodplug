6b5ba128625205fac2d5f10b1f9307228472ff32
Antidote-cell <arotoluj@gmail.com>
2022-10-14 19:43:42 +0800

    completed hamburger

--- starter/main.js
+++ starter/main.js
@@ -7,11 +7,14 @@
 // console.log (document)
 
 var hamburgerdiv = document.querySelector(".hamburger")
+var mobilelinks = document.querySelector(".mobile-link-holder")
+var mybackdrop = document.querySelector(".backdrop")
 
-console.log(hamburgerdiv)
 
 
 function dosomething() {
     hamburgerdiv.classList.toggle("showburger")
+    mobilelinks.classList.toggle("show-mobile-link-holder")
+    mybackdrop.classList.toggle("show-backdrop")
 }
 
